d2b49502b152a44049a3aced08e34824acf251b3
nehavaghasiya1502 <nehavaghasiya573@gmail.com>
2026-01-23 13:56:00 +0530

    upload files

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,31 +1,3 @@
-// import { useContext } from "react";
-// import { ThemeContext } from "../Context/ThemeContext";
-
-// const Sidebar = () => {
-//   const { theme, toggleTheme } = useContext(ThemeContext);
-
-//   return (
-//     <aside className={`sidebar ${theme}`}>
-//       <h1 className="logo">Admin</h1>
-
-//       <nav>
-//         <a className="nav-item active">Users</a>
-//         <a className="nav-item">Products</a>
-//         <a className="nav-item">Orders</a>
-//       </nav>
-
-//       <div className="theme-box">
-//         <span>{theme.toUpperCase()}</span>
-//         <label className="switch">
-//           <input type="checkbox" onChange={toggleTheme} />
-//           <span className="slider"></span>
-//         </label>
-//       </div>
-//     </aside>
-//   );
-// };
-
-// export default Sidebar;
 import { useContext } from "react";
 import { ThemeContext } from "../Context/ThemeContext";
 
